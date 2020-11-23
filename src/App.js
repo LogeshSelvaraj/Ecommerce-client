c09e7524e4f47a0e1fafcd45d64aef5b1c595957
LogeshSelvaraj <logeshksr99@gmail.com>
2020-11-23 11:19:23 +0530

    app use effect stored seperately

--- src/App.js
+++ src/App.js
@@ -22,49 +22,7 @@ import {UpdatingStates} from './functions/UpdatingStates'
 function App() {
 
       UpdatingStates();
-
-  // const dispatch = useDispatch()
-
-
-  //   const roleBasedRedirect = (role) => {
-  //     if (role === "admin") {
-  //       dispatch({
-  //         type: "LOGINAS_ADMIN",
-  //       });
-  //     } else {
-  //       dispatch({
-  //         type: "LOGINAS_USER",
-  //       });
-  //     }
-  //   };
-
-  // useEffect( ()=>{
-  //   const unsubcribe=auth.onAuthStateChanged(async (user)=>{
-  //       if(user){
-  //         const idToken = (await user.getIdTokenResult()).token;
-  //   getUser(idToken)
-  //    .then((res) => {
-  //      roleBasedRedirect(res.data.role);
-  //      dispatch({
-  //        type: "LOGIN_USER",
-  //        payload: {
-  //          name: res.data.name,
-  //          email: res.data.email,
-  //          token: idToken,
-  //          role: res.data.role,
-  //          _id: res.data.id,
-  //        },
-  //      });
-       
-  //    })
-  //    .catch((err) => console.log(err));
-
-  //       }
-  //   })
-  //   return () => unsubcribe()
-  // },[])
-
-
+      
   return (
     <>
       <NavBar />
